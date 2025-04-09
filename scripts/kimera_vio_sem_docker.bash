5d0768f59ce84dbee1658aac3f6398a826009c1e
Aurunima <112366204+Aurunima@users.noreply.github.com>
2025-04-09 13:01:40 +0530

    USB access for realsense camera
    For the realsense camera to be able to be accessed from within the container

--- scripts/kimera_vio_sem_docker.bash
+++ scripts/kimera_vio_sem_docker.bash
@@ -2,11 +2,19 @@
 
 # Allow X server connection
 # xhost +local:root
-docker run -it \
+docker run --memory=8g --memory-swap=8g -it \
+    --privileged \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
+    --device=/dev/bus/usb \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -v /etc/udev/rules.d:/etc/udev/rules.d \
+    -v /lib/udev:/lib/udev \
+    -v /run/udev:/run/udev \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/home/art5gpc6/datasets/:/datasets/" \
     --name kimera_vio_sem kimera_vio_sem_ros
 # Disallow X server connection
 # xhost -local:root
+
+# Use --rm if you wish for the container to be removed once you exit it
